3fbef1ce3a9975fdb8c4b236802e30f2b37cc574
Prashant Rajaji <103595753+Hunter-Prash@users.noreply.github.com>
2025-04-04 01:39:37 +0530

    add login functionality with JWT authentication

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,6 +1,10 @@
 import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
 import { userModel } from '../models/userModel.js'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 export const signUpController=async(req,res)=>{
     const{name,email,password,answer}=req.body
@@ -25,6 +29,35 @@ export const signUpController=async(req,res)=>{
         res.status(500).json({
             message:'internal server error'
         })
+    }
+}
+
+export const loginController=async(req,res)=>{
+    try{
+        const {email,password}=req.body
+        
+    if(!email || !password){
+        return res.status(401).json({message:'Fill all fields'})
+    }
+    const user=await userModel.findOne({email})
+
+    if(!user){
+        return res.status(400).json({message:'user not found'})
+    }
+
+    const match=await bcrypt.compare(password,user.password)
+    if(!match){
+        return res.status(400).json({message:'Invalid credentials'})
+    }
+
+    const token=jwt.sign({_id:user._id},process.env.JWT_SECRET,{expiresIn:'1d'})
+    res.status(200).json({
+        token:token,
+        user:user
+    })
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message:'Internal server error'})
     }
 }
 
--- Server/routes/authRoute.js
+++ Server/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
 
-import { signUpController } from "../controllers/authController.js";
+import { loginController, signUpController } from "../controllers/authController.js";
 
 const router=express.Router()
 
 router.post('/signup',signUpController)
+router.post('/login',loginController)
 
 export default router;
